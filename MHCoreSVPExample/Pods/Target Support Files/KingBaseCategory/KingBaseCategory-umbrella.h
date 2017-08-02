d5ccceb546fce7f54f93987069a380bd9d95a578
hengyangKing <hengyangKing.yeah.net>
2017-08-02 13:59:13 +0800

    add fastlane

--- MHCoreSVPExample/Pods/Target Support Files/KingBaseCategory/KingBaseCategory-umbrella.h
+++ MHCoreSVPExample/Pods/Target Support Files/KingBaseCategory/KingBaseCategory-umbrella.h
@@ -4,12 +4,6 @@
 
 #import "NSObject+MHJDeviceTools.h"
 #import "UIFont+MHJFont.h"
-#import "UIImage+Color.h"
-#import "UIImage+Cut.h"
-#import "UIImage+FixOrientation.h"
-#import "UIImage+KingExtension.h"
-#import "UIImage+MHBundleImage.h"
-#import "UIImage+Water.h"
 
 FOUNDATION_EXPORT double KingBaseCategoryVersionNumber;
 FOUNDATION_EXPORT const unsigned char KingBaseCategoryVersionString[];
